fcee7d52b53c8e7c009c9c6dce3323b9a3456ab8
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2022-07-23 11:12:13 +0600

    Solve
    50A-https://codeforces.com/problemset/submission/50/165386202
    158A-https://codeforces.com/problemset/submission/158/165385995

---
+++ LAB-02/next-round-158A (3).cpp
@@ -0,0 +1,20 @@
+//cf problem no- 158A
+// problem name- next round
+//codeforce solve-https://codeforces.com/problemset/submission/158/165385995
+//id-334
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,k,a[100],i,sum=0; 
+cin>>n>>k; 
+for(i=0; i<n; i++) 
+{ cin>>a[i]; } 
+for(i=0; i<n; i++) 
+{ 
+if(a[i]>=a[k-1] && a[i]>0) 
+sum++; 
+} 
+cout<<sum;
+return 0;
+}
