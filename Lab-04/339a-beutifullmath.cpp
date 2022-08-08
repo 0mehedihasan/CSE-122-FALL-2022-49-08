92721b7a0ea57b26c4e72b7f74b300631db50f8b
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2022-08-08 22:10:27 +0600

    Add files via upload

---
+++ Lab-04/339a-beutifullmath.cpp
@@ -0,0 +1,23 @@
+//prob-339A
+//problem-solution-https://codeforces.com/problemset/submission/339/167536482
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string s;
+cin>>s;
+int i,j;
+for(i=0; i<s.size(); i+=2)
+{
+for(j=0; j<s.size()-1; j+=2) 
+{
+if(s[j]>s[j+2])
+{
+swap(s[j],s[j+2]);
+}
+}
+} 
+cout<<s<<endl; 
+return 0;
+}
